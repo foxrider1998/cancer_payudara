6bf28a87f9267a418698302dd51201230a437ac4
astea13 <hptitih@gmail.com>
2022-12-04 18:43:11 +0700

    test

--- webapp/app_classificacao.py
+++ webapp/app_classificacao.py
@@ -7,7 +7,7 @@ import time
 
 
 #importar arquivos
-df = pd.read_csv('dataset_tratado.csv', sep=',')
+df = pd.read_csv('/dataset_tratado.csv', sep=',')
 df_class2 = df[df['Class'] == 2]
 df_class4 = df[df['Class'] == 4]
 
